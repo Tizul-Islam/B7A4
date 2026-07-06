ff65eb19af9ae953ffd7fef2a004f77f8a6a838e
tizul-islam <tizulislamtt@gmail.com>
2026-07-06 22:42:16 +0600

    Server is running on port 5001

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import cookieParser from "cookie-parser";
+
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+
+
+
+
+
+
+
+
+// app.use("/api/subscription/webhook",express.raw({ type: "application/json" }))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+// app.get("/premium", (req: Request, res: Response) => {
+//   res.send("<h1>Subscription Successful!</h1><p>Thank you for subscribing. You can now access premium content.</p>");
+// });
+
+// app.get("/payment", (req: Request, res: Response) => {
+//   res.send("<h1>Subscription Cancelled/Failed</h1><p>The checkout process was cancelled or failed. Please try again.</p>");
+// });
+
+// // Routes
+// app.use("/api/users", userRouter);
+// app.use("/api/auth", authRouter);
+// app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/subscription", subscriptionRouter);
+// app.use("/api/premium",premiumRoutes);
+// // Not Found Handler
+// app.use(notFound);
+
+// // Global Error Handler
+// app.use(globalErrorHandler);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+
+import "dotenv/config";
+
+const PORT = process.env.PORT || 5001;
+
+
+async function main() {
+    try{
+   
+        console.log("Connected to the database successfully.");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+  
+        process.exit(1);
+    }
+}
+
+main();
